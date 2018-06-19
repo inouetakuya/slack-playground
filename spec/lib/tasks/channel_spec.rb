dcf49811fe0cce1546d3e4707118cff595b4ac42
INOUE Takuya <inouetakuya5@gmail.com>
2018-06-19 13:03:13 +0900

    + FileUtils.rm_rf(Dir[File.join(SlackPlayground::CHANNELS_DIR, '*')])

--- spec/lib/tasks/channel_spec.rb
+++ spec/lib/tasks/channel_spec.rb
@@ -1,5 +1,9 @@
 describe Task::Channel do
   describe '#history' do
+    after do
+      FileUtils.rm_rf(Dir[File.join(SlackPlayground::CHANNELS_DIR, '*')])
+    end
+
     it '正常終了する' do
       expect {
         Task::Channel.new.invoke(:history, [], channel: '#livesense-engineers')
